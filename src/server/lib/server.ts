eb80e1c90397e9a2646f2914ff04d698cd625ae5
Alberto <alberto.godinez90@gmail.com>
2018-07-02 21:18:11 -0500

    changed port for heroku

--- src/server/lib/server.ts
+++ src/server/lib/server.ts
@@ -1,5 +1,5 @@
-import app from "./app";
-const port = 4040;
+import app from './app';
+const port = process.env.PORT || 4040;
 app.listen(port, function() {
   console.log('Express server listening on port ' + port);
 });
