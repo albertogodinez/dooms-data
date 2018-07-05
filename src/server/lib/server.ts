0577a59d378d1855959dff13677847d38fa2a040
Alberto <alberto.godinez90@gmail.com>
2018-07-04 20:37:22 -0500

    heroku build

--- src/server/lib/server.ts
+++ src/server/lib/server.ts
@@ -2,6 +2,9 @@ import app from './app';
 const path = require('path');
 const port = process.env.PORT || 4040;
 
+app.get('*', function(req, res) {
+  res.sendFile(path.resolve(__dirname, 'public/index.html'));
+});
 app.listen(port, function() {
   console.log('Express server listening on port ' + port);
 });
