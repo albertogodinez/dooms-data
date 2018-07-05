174aee1cfeb2569e6622350d0919ce36b506ba2a
Alberto <alberto.godinez90@gmail.com>
2018-07-05 12:09:59 -0500

    heroku build

--- src/server/lib/app.ts
+++ src/server/lib/app.ts
@@ -32,12 +32,6 @@ class App {
   private routes(): void {
     const router = express.Router();
 
-    // router.get('/', (req: Request, res: Response) => {
-    //   res.status(200).send({
-    //     message: 'Hello World!'
-    //   });
-    // });
-
     router.get(
       '/tournaments/:username/:apiKey',
       (req: Request, res: Response) => {
