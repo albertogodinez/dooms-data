e7ac29236e7f90526d7b088f135895d090997dfd
Alberto <alberto.godinez90@gmail.com>
2018-07-04 22:53:42 -0500

    heroku build

--- src/server/lib/server.ts
+++ src/server/lib/server.ts
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path');
 const appRoot = require('app-root-path');
 const port = process.env.PORT || 4040;
-app.use(express.static(path.join(appRoot + '/build')));
+app.use(express.static(path.join(appRoot + '/build'), { index: false }));
 console.log('path joined: ' + path.join(appRoot + '/build'));
 app.get('/', function(request, response) {
   response.sendFile(appRoot + '/build/index.html');
