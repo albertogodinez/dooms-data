05e2ee540a72619252064fa2628815c5abe6a5e4
Alberto <alberto.godinez90@gmail.com>
2018-07-05 07:15:37 -0500

    heroku build

--- src/server/lib/server.ts
+++ src/server/lib/server.ts
@@ -7,7 +7,6 @@ app.use(express.static(path.join(appRoot + '/build'), { index: false }));
 console.log('path joined: ' + path.join(appRoot + '/build'));
 app.get('/', function(request, response) {
   response.sendFile(appRoot + '/build/index.html');
-  // response.sendFile(path.resolve(appRoot, 'build', 'index.html'));
 });
 app.listen(port, function() {
   console.log('Express server listening on port ' + port);
