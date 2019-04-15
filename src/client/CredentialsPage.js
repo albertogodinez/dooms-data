bf1e1f79532693a519e2a14be43505056fccdbf7
Alberto <alberto.godinez90@gmail.com>
2019-04-15 18:57:54 -0500

    - updated webpack-cli - Added a demo button for quick access

--- src/client/CredentialsPage.js
+++ src/client/CredentialsPage.js
@@ -14,9 +14,10 @@ export default class CredentialsPage extends Component {
     super(props);
     this.state = {
       username: '',
-      apiKey: '',
+      apiKey: ''
     };
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.setData = this.setData.bind(this);
   }
 
   componentDidMount() {
@@ -24,31 +25,45 @@ export default class CredentialsPage extends Component {
     this.props.form.validateFields(); // automatically sets to props by Form.create()
   }
 
+  setData() {
+    this.setState({
+      username: 'ATXSanctumWeeklies',
+      apiKey: 'WEuxMDp0NG3rHxWMjYpURprGhWUyEgtTlGEm251I'
+    });
+  }
+
   handleSubmit(event) {
     event.preventDefault();
     this.props.form.validateFields((err, values) => {
       if (!err) {
-        this.state.username = values.username;
-        this.state.apiKey = values.apiKey;
+        this.setState({
+          username: values.username,
+          apiKey: values.apiKey
+        });
+        // this.state.username = values.username;
+        // this.state.apiKey = values.apiKey;
         console.log('Received values of form: ', values);
       }
     });
 
-    const rootUrl = process.env.NODE_ENV === 'production' ? '/api/' : 'http://localhost:4040/api/';
+    const rootUrl =
+      process.env.NODE_ENV === 'production'
+        ? '/api/'
+        : 'http://localhost:4040/api/';
     axios({
       url: `${rootUrl}tournaments/${this.state.username}/${this.state.apiKey}`,
       method: 'get',
       headers: {
         begDate: '2018-01-01', // don't hard code this in the future
-        endDate: '2018-07-01', // don't hard code this in the future
-      },
+        endDate: '2018-07-01' // don't hard code this in the future
+      }
     })
-      .then((response) => {
+      .then(response => {
         if (response.data) {
           this.props.changeView(response.data);
         }
       })
-      .catch((ex) => {
+      .catch(ex => {
         // TODO: Convert this into it's own component
         alert(`error occurred. Refresh and try again: ${JSON.stringify(ex)}`);
       });
@@ -56,36 +71,67 @@ export default class CredentialsPage extends Component {
 
   render() {
     const {
-      getFieldDecorator, getFieldsError, getFieldError, isFieldTouched,
+      getFieldDecorator,
+      getFieldsError,
+      getFieldError,
+      isFieldTouched
     } = this.props.form;
 
     // Only show error after a field is touched.
-    const userNameError = isFieldTouched('username') && getFieldError('username');
+    const userNameError =
+      isFieldTouched('username') && getFieldError('username');
     const apiError = isFieldTouched('apiKey') && getFieldError('apiKey');
     return (
       <div>
         <Row>
           <Col span={8} offset={8}>
             <Form layout="inline" onSubmit={this.handleSubmit}>
-              <FormItem validateStatus={userNameError ? 'error' : ''} help={userNameError || ''}>
+              <FormItem
+                validateStatus={userNameError ? 'error' : ''}
+                help={userNameError || ''}
+              >
                 {getFieldDecorator('username', {
-                  rules: [{ required: true, message: 'Please input your username' }],
-                })(<Input
-                  prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                  placeholder="Username"
-                />)}
+                  rules: [
+                    { required: true, message: 'Please input your username' }
+                  ]
+                })(
+                  <Input
+                    prefix={
+                      <Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />
+                    }
+                    placeholder="Username"
+                  />
+                )}
               </FormItem>
-              <FormItem validateStatus={apiError ? 'error' : ''} help={apiError || ''}>
+              <FormItem
+                validateStatus={apiError ? 'error' : ''}
+                help={apiError || ''}
+              >
                 {getFieldDecorator('apiKey', {
-                  rules: [{ required: true, message: 'Please input your API key' }],
-                })(<Input
-                  prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                  placeholder="API Key"
-                />)}
+                  rules: [
+                    { required: true, message: 'Please input your API key' }
+                  ]
+                })(
+                  <Input
+                    prefix={
+                      <Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />
+                    }
+                    placeholder="API Key"
+                  />
+                )}
               </FormItem>
               <FormItem>
-                <Button type="primary" htmlType="submit" disabled={hasErrors(getFieldsError())}>
+                <Button
+                  type="primary"
+                  htmlType="submit"
+                  disabled={hasErrors(getFieldsError())}
+                >
                   Submit
+                </Button>
+              </FormItem>
+              <FormItem>
+                <Button type="primary" htmlType="submit" onClick={this.setData}>
+                  Quick Demo? Press Here
                 </Button>
               </FormItem>
             </Form>
@@ -97,11 +143,11 @@ export default class CredentialsPage extends Component {
 }
 
 CredentialsPage.defaultProps = {
-  changeView: null,
+  changeView: null
 };
 
 CredentialsPage.propTypes = {
-  changeView: PropTypes.func,
+  changeView: PropTypes.func
 };
 
 export const CredentialsPageForm = Form.create()(CredentialsPage);
